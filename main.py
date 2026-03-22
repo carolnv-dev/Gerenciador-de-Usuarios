4e2f9ed9db4586b53d12cc06e44d0d810dfdda0c
Caroline <novaskicaroline08@gmail.com>
2026-03-21 21:18:33 -0300

    atualiza codigo

--- main.py
+++ main.py
@@ -1,2 +1,29 @@
-# projeto leitor de usuários
+from collections import Counter
 
+nomes = []
+idades = []
+cpfs = []
+
+programa = 0
+while programa == 0:
+    comando = input('Digite ADD para adicionar um usuário ou END para finalizar: ').upper()
+
+    if comando == 'ADD':
+        nomes.append(input(f'Digite o nome: '))
+        idades.append(int(input(f'Digite a idade: ')))
+        cpf = input('Digite o CPF:')
+
+        if len(cpf) < 14:
+            print('CPF inválido! Tente novamente.')
+            del nomes[-1]
+            del idades[-1]
+        else:
+            cpfs.append(cpf)
+
+    elif comando == 'END':
+        print(f'A média das idades é {sum(idades) / len(idades)}')
+        contagem = Counter(''.join(nomes).upper())
+        print(f'As letras que mais apareceram foram: {contagem.most_common(3)}')
+        programa = 1
+    else:
+        print('Você digitou um comando inválido! Tente novamente.')
