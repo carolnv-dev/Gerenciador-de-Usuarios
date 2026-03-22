477301a378c0169500628227400156edb3eac4e5
Caroline <novaskicaroline08@gmail.com>
2026-03-22 16:45:08 -0300

    atualiza codigo

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 from collections import Counter
 
-nomes = []
-idades = []
-cpfs = []
-
+usuarios = []
 programa = 0
 while programa == 0:
-    comando = input('Digite ADD para adicionar um usuário ou END para finalizar: ').upper()
+    comando = input('Digite ADD para adicionar um usuário e END para finalizar o programa: ').upper()
 
     if comando == 'ADD':
-        nomes.append(input(f'Digite o nome: '))
-        idades.append(int(input(f'Digite a idade: ')))
-        cpf = input('Digite o CPF:')
+        nome = input('Nome: ')
+        idade = int(input('Idade: '))
+        cpf = input('CPF: ')
 
         if len(cpf) < 14:
             print('CPF inválido! Tente novamente.')
-            del nomes[-1]
-            del idades[-1]
         else:
-            cpfs.append(cpf)
+            usuario = {
+                'nome': nome,
+                'idade': idade,
+                'cpf': cpf
+            }
+            usuarios.append(usuario)
 
     elif comando == 'END':
-        print(f'A média das idades é {sum(idades) / len(idades)}')
-        contagem = Counter(''.join(nomes).upper())
-        print(f'As letras que mais apareceram foram: {contagem.most_common(3)}')
+
+        soma_idades = 0
+        usuarios_nomes = ''
+        for u in usuarios:
+
+            # soma todas as idades:
+            soma_idades += u['idade']
+
+            # concatena todos os nomes:
+            usuarios_nomes += u['nome']
+
+        # mostra a média das idades na tela
+        print(f'A média das idades é: {soma_idades / len(usuarios)}')
+
+        # mostra a incidência das 3 letras que mais aparecem na tela
+        contagem_letras = Counter(usuarios_nomes.upper())
+        print(f'As letras com maior incidência foram: {contagem_letras.most_common(3)}')
+
+        # finaliza o loop
         programa = 1
     else:
-        print('Você digitou um comando inválido! Tente novamente.')
+        print('Comando inválido! Tente novamente.')
